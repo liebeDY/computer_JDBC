3064aa5161a37aa8eab7b02e4f2e8e1e1c948cc0
liebeDY <hdydeveloper0927@gmail.com>
2020-12-04 15:53:19 +0900

    수업#6 2020.12.04.금요일 #17 [JDBC 예제 4_update]

--- 17Day_JDBC/src/step4/TestJDBC3.java
+++ 17Day_JDBC/src/step4/TestJDBC3.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TestJDBC4 {
+public class TestJDBC3 {
 
 	public static void main(String[] args) {
 
