ce8abc778e2c27a5f0c5e7a750497875f54dde12
liebeDY <hdydeveloper0927@gmail.com>
2020-12-04 15:52:13 +0900

    수업#6 2020.12.04.금요일 #17 [JDBC 예제 4_update]

---
+++ 17Day_JDBC/src/step4/TestJDBC4.java
@@ -0,0 +1,37 @@
+package step4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestJDBC4 {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		Statement stmt = null;
+		
+		String jdbc_driver = "oracle.jdbc.driver.OracleDriver";
+		String jdbc_url = "jdbc:oracle:thin:@localhost:1521:xe";
+		
+		try {
+			Class.forName(jdbc_driver);
+			conn = DriverManager.getConnection(jdbc_url, "hr", "hr");
+			System.out.println("jdbc driver loading ok");
+			stmt = conn.createStatement();
+			
+			String sql = "update member set address = '¼ö¼­' where name = '±è¿¬¾Æ'";
+			int result = stmt.executeUpdate(sql);
+			System.out.println(result);
+			
+			stmt.close();
+			conn.close();
+			
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 	
+	}
+}
